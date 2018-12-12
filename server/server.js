363bf8c5dc7ab14638944799abbd06df3a0aadaa
Bhavya Srivastava <bhavya17037@iiitd.ac.in>
2018-12-12 11:41:29 +0530

    Make PATCH /todos/:id API endpoint

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var { ObjectID } = require("mongodb");
+const _ = require("lodash");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 var { mongoose } = require("./db/mongoose.js");
 var { Todo } = require("./models/todo.js");
@@ -72,6 +73,41 @@ app.delete("/todos/:id", (req, res) => {
         res.status(404).send();
       }
 
+      res.send({
+        todo: todo
+      });
+    })
+    .catch(err => {
+      res.status(400).send();
+    });
+});
+
+app.patch("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ["text", "completed"]);
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(
+    id,
+    {
+      $set: body
+    },
+    { new: true }
+  )
+    .then(todo => {
+      if (!todo) {
+        res.status(404).send();
+      }
       res.send({
         todo: todo
       });
